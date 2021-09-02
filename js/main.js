597a3545e501e974c06be79f4f067e27ee59dabe
sojibdebnath <sojibdebnath.cse@gmail.com>
2021-09-02 20:40:05 +0600

    total added

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ const searchBook = () => {
     searchResult.innerHTML = '';
     bookHit.innerHTML = '';
 
-    const url = ` http://openlibrary.org/search.json?q=${searchText} `;
+    const url = ` https://openlibrary.org/search.json?q=${searchText} `;
     fetch(url)
         .then(res => res.json())
         .then(data => displaySearchResult(data));
@@ -30,25 +30,27 @@ const displaySearchResult = data => {
     else {
         errorDiv.innerText = '';
     }
-    const docs = data.docs;
+    const arrayImage = data.docs;
+    const docs = arrayImage.slice(0, 12);
     const numFound = data.numFound;
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = `
         <h4 class="text-center">Total Book: ${numFound} </h4>
         `;
-
+    // Loop through
     docs.forEach((doc) => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card">
           <div class="d-flex justify-content-center">
-            <img class=" w-50 h-50 p-2" src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card - img - top" alt="...">
+            <img class=" w-60 h-60 p-2" src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card - img - top" alt="...">
            </div >
     <div class="card-body">
-        <h3 class="card-title">Book Name: ${doc.title}</h3>
+        <h4 class="card-title">Book Name: <span class="text-danger">${doc.title}</span></h4>
         <h6 class="card-title">Author Name: ${doc.author_name}</h6>
+        <h6 class="card-title">publisher: ${doc.publisher}</h6>
         <h6 class="card-title">First Publish Year: ${doc.first_publish_year ? doc.first_publish_year : ''}</h6>
     </div>
         </div >
